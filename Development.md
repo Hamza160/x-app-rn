39cf352d78fb3d794305e2cda3d56d147249f755
HamzaAshraf160 <hamza.ashraf@cybersoph.com>
2025-09-12 20:17:14 +0500

    Prisma Added

--- Development.md
+++ Development.md
@@ -1,1 +1,4 @@
+ExpressJs Setup
 https://blog.logrocket.com/express-typescript-node/
+Prisma Setup
+https://medium.com/@aviralj02/setting-up-express-with-typescript-adding-prisma-for-database-operations-b7ccea552588
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
-import config from './config/config'
-import express, {type Express} from 'express'
+import "dotenv/config"
+import express, {Express} from "express";
 
-const app: Express = express()
+const app:Express = express();
 
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
-
-app.listen(config.port, () => console.log(`Server started on port http://localhost:${config.port}`))
+app.listen(PORT, () => {
+    console.log(`Server started on port http://localhost:${PORT}`);
+});
