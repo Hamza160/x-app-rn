f26f73c11b3d52f30a961c536499e60544c22511
HamzaAshraf160 <hamza.ashraf@cybersoph.com>
2025-09-15 05:19:25 +0500

    Middlewares Added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,22 @@
+
 import {ENV} from "./config/env";
-import express, {Express, Request, Response} from "express";
+import express, {type Express, Request, Response} from "express";
+import cors from "cors";
+import {clerkMiddleware} from '@clerk/express'
+import userRoutes from "./routes/user.route";
 
 const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(clerkMiddleware())
+
+// Routes
+app.use('/api/users', userRoutes)
 
 app.get("/", (req: Request, res: Response) => {
     return res.send("Hello World!");
 });
 
-app.listen(9000, () => console.log(`Server started on port http://localhost:${ENV.PORT}`));
+app.listen(ENV.PORT, () => console.log(`Server started on port http://localhost:${ENV.PORT}`));
