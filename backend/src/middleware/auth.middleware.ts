e6d98bf1cb20d85d644c72e2722b032243ec095c
HamzaAshraf160 <hamza.ashraf@cybersoph.com>
2025-09-19 03:45:27 +0500

    Final Push Done

--- backend/src/middleware/auth.middleware.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response, NextFunction} from "express";
-
-export const protectRoute = async (req:Request, res:response, next:NextFunction) => {
-    if(!req.auth().isAuthenticated()) {
-        return res.status(401).json({message: 'Not authorized'});
-    }
-    next();
-}
-
